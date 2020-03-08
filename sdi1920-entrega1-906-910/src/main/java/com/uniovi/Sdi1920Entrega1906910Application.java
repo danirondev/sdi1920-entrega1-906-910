1d2a53ce7a8a8f637adeac3c857fa0929d79ad8d
UO263745 <uo263745@uniovi.es>
2020-03-08 23:43:13 +0100

    Creacion del proyecto inicial sdi1920-entrega1-906-910.

---
+++ sdi1920-entrega1-906-910/src/main/java/com/uniovi/Sdi1920Entrega1906910Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1920Entrega1906910Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi1920Entrega1906910Application.class, args);
+	}
+
+}
