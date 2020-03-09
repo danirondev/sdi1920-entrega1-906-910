0cb206a45f50581374ed7d80e30f4299df804a2f
UO263745 <uo263745@uniovi.es>
2020-03-09 10:50:52 +0100

    commit prueba 4 corregir vistas

--- sdi1920-entrega1-906-910/src/main/java/com/uniovi/repositories/UsersRepository.java
+++ sdi1920-entrega1-906-910/src/main/java/com/uniovi/repositories/UsersRepository.java
@@ -3,5 +3,6 @@ package com.uniovi.repositories;
 import com.uniovi.entities.*;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface UsersRepository extends CrudRepository<Usuario, Long>{
 }
