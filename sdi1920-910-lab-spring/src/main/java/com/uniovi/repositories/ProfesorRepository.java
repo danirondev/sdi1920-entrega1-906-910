c5e6e5a11e988e10a53b9f45ba7da7db0632ca16
Noé Fernández Moro <uo251683@uniovi.es>
2020-03-13 13:06:14 +0100

    Eliminada carpeta sobrante

--- sdi1920-910-lab-spring/src/main/java/com/uniovi/repositories/ProfesorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uniovi.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.uniovi.entities.Profesor;
-
-public interface ProfesorRepository extends CrudRepository<Profesor, Long>{
-}
