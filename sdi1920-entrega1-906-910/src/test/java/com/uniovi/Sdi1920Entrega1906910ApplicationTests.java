84cd0a0122d45790df87220a005968eab3c84d61
Noé Fernández Moro <uo251683@uniovi.es>
2020-03-13 17:53:14 +0100

    Pruebas1 preparadas

--- sdi1920-entrega1-906-910/src/test/java/com/uniovi/Sdi1920Entrega1906910ApplicationTests.java
+++ sdi1920-entrega1-906-910/src/test/java/com/uniovi/Sdi1920Entrega1906910ApplicationTests.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.uniovi.pageobjects.PO_HomeView;
+import com.uniovi.pageobjects.PO_Properties;
+
 import org.junit.runners.MethodSorters; 
 
 //Ordenamos las pruebas por el nombre del método 
@@ -42,16 +46,37 @@ public class Sdi1920Entrega1906910ApplicationTests {
 		
 	}
 	
-	//Al finalizar la última prueba 
-	@AfterClass 
+	//PR01. Acceder a la página principal /  
+	@Test  
+	public void PR01() {  
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH()); 
+	} 
+	
+	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home 
 	@Test
-	static public void end() { 
-		//Cerramos el navegador al finalizar las pruebas 
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+	
+	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home 
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+	
+	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español 
+	@Test  
+	public void PR04() {   
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(), PO_Properties.getENGLISH()); 
+		//SeleniumUtils.esperarSegundos(driver, 2); 
 		driver.quit(); 
 	}
 	
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	//Al finalizar la última prueba 
+//	@AfterClass 
+//	@Test
+//	static public void end() { 
+//		//Cerramos el navegador al finalizar las pruebas 
+//		driver.quit(); 
+//	}
 }
