ea33f2902669e5fca62e147566314a69c9caccee
Noé Fernández Moro <uo251683@uniovi.es>
2020-03-13 16:20:42 +0100

    Pruebas1 - Base para vistas Base para probar la vista Home

--- sdi1920-entrega1-906-910/src/test/java/com/uniovi/Sdi1920Entrega1906910ApplicationTests.java
+++ sdi1920-entrega1-906-910/src/test/java/com/uniovi/Sdi1920Entrega1906910ApplicationTests.java
@@ -1,13 +1,57 @@
 package com.uniovi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.runners.MethodSorters; 
 
-@SpringBootTest
-class Sdi1920Entrega1906910ApplicationTests {
+//Ordenamos las pruebas por el nombre del método 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) 
 
+public class Sdi1920Entrega1906910ApplicationTests {
+
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizaciones automáticas)): 
+	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"; 
+	static String Geckdriver024 = "C:\\Path\\geckodriver024win64.exe"; 
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024); 
+	static String URL = "http://localhost:8090";
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	//Antes de cada prueba se navega al URL home de la aplicaciónn 
+	@Before
+	public void setUp() throws Exception {
+		driver.navigate().to(URL); 
+	}
+
+	//Después de cada prueba se borran las cookies del navegador 
+	@After
+	public void tearDown() throws Exception {
+		driver.manage().deleteAllCookies(); 
+	}
+
+	//Antes de la primera prueba   
+	@BeforeClass 
+	static public void begin() { 
+		
+	}
+	
+	//Al finalizar la última prueba 
+	@AfterClass 
 	@Test
-	void contextLoads() {
+	static public void end() { 
+		//Cerramos el navegador al finalizar las pruebas 
+		driver.quit(); 
+	}
+	
+	public void test() {
+		fail("Not yet implemented");
 	}
 
 }
---
+++ sdi1920-entrega1-906-910/src/test/java/com/uniovi/pageobjects/PO_RegisterView.java
@@ -0,0 +1,5 @@
+package com.uniovi.pageobjects;
+
+public class PO_RegisterView {
+
+}
